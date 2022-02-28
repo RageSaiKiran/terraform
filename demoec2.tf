8ec923d124971e817519ed0631c9979638192424
Rage Sai Kiran <70426630+RageSaiKiran@users.noreply.github.com>
2022-02-28 18:26:19 +0530

    Update demoec2.tf

--- demoec2.tf
+++ demoec2.tf
@@ -3,6 +3,22 @@ provider "aws" {
   access_key = "AKIAU3OTFLNWRLJ6EVUZ"
   secret_key = "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"
 }
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAU3OTFLNWRLJ6EVUZ"
+  secret_key = "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"
+}
+
+
+##
+resource "aws_instance" "web" {
+  ami           ="ami-033b95fb8079dc481"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ubuntu"
+  }
+}
 
 
 ##
